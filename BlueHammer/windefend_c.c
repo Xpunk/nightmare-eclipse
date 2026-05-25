136c00b4908df0c09b9ed4903f2bea2e335c6a5d
nctu6 <nctu6.tw@gmail.com>
2026-05-25 15:47:26 +0800

    https://gitlab.com/nightmare-eclipse

---
+++ BlueHammer/windefend_c.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c421080690de885a232a1aeb9df2a18ce9119e13b8ea110c5171803371b29a4
+size 1975946
---
+++ BlueHammer/windefend_s.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b43e026f63d47df0109d3300509c7943b659e6687e3a1e26040995550b6b32a
+size 1869017
